269ac549cf26b58a7dda1e629d4ea89d30ae1d28
G B <egor.workroom@gmail.com>
2016-06-12 11:34:33 +0600

    fix options test

--- store_local_test.go
+++ store_local_test.go
@@ -1,26 +1,16 @@
 package dbox
 
 import (
-	"io/ioutil"
 	"os"
 	"testing"
-	// "github.com/stretchr/testify/assert"
 )
 
 func TestLocalStore_simpleStrategy(t *testing.T) {
-	store := NewLocalStore("/Users/gbv/work/src/dbox/_testdata/")
+	store := NewLocalStore("./_testdata/")
 
 	createSimpleStrategy(t, store)
 
 	deleteSimpleStrategy(t, store)
-}
 
-func TestLocalStore_writereadFile(t *testing.T) {
-	ioutil.WriteFile("testfile", []byte("text"), 0644)
-	ioutil.WriteFile("testfile", []byte("text text"), 0644)
-	b, _ := ioutil.ReadFile("testfile")
-	if string(b) != "text text" {
-		t.Error("not expected value")
-	}
-	os.Remove("testfile")
+	os.RemoveAll("./_testdata/")
 }
