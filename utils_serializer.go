2917e54e70322cd5f4698840d3d1dda4d05cf69c
G B <egor.workroom@gmail.com>
2016-06-25 20:16:40 +0600

    decoder refactoring (supported map[interface{}]interface{})

--- utils_serializer.go
+++ utils_serializer.go
@@ -1,6 +1,7 @@
 package dbox
 
 import (
+	"bytes"
 	"gopkg.in/vmihailenco/msgpack.v2"
 )
 
@@ -12,6 +13,30 @@ func encode(v interface{}) ([]byte, error) {
 }
 
 func decode(v interface{}, b []byte) error {
+	dec := msgpack.NewDecoder(bytes.NewBuffer(b))
+	dec.DecodeMapFunc = func(d *msgpack.Decoder) (interface{}, error) {
+		n, err := d.DecodeMapLen()
+		if err != nil {
+			return nil, err
+		}
 
-	return msgpack.Unmarshal(b, v)
+		m := make(map[string]interface{}, n)
+		for i := 0; i < n; i++ {
+			mk, err := d.DecodeString()
+			if err != nil {
+				return nil, err
+			}
+
+			mv, err := d.DecodeInterface()
+			if err != nil {
+				return nil, err
+			}
+
+			m[mk] = mv
+		}
+		return m, nil
+	}
+
+	return dec.Decode(v)
+	// return msgpack.Unmarshal(b, v)
 }
